71d5fc2eca567cd3b4899815bc944d44a712616b
Edin Dudojević <edudojevic@users.noreply.github.com>
2019-04-05 16:36:23 +0200

    add searching for topic

--- dialogflow-middleware.js
+++ dialogflow-middleware.js
@@ -18,13 +18,33 @@ const getMostPopularSession = async (apiToken) => {
     return {titleOfSession: mostPopularSession.title, numberOfAttendees: mostPopularSession.number_of_attendees};
 };
 
+const getTopicTitle = async (apiToken, topic) => {
+    console.log(topic);
+
+    const result = await(await fetch(`https://xke.xebia.com/api/session/?xke=2019-04-16&search=${topic}`, {
+        headers: {
+            'Authorization': `Token ${apiToken}`
+        }
+    })).json();
+
+
+    if (result.length) {
+        return result[0].title;
+    }
+
+    return undefined;
+};
+
 module.exports.getMostPopularSession = getMostPopularSession;
+module.exports.getTopicTitle = getTopicTitle;
 
 module.exports = (request, response) => {
+
+
     const API_TOKEN = request.get('API_TOKEN');
     const agent = new WebhookClient({request, response});
-    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    // console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    // console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
     function fallback(agent) {
         agent.add(`I didn't understand`);
@@ -36,9 +56,21 @@ module.exports = (request, response) => {
         agent.add(`The most popular XKE session is "${titleOfSession}" with ${numberOfAttendees} attendees.`)
     }
 
+    async function searchTopic() {
+        console.log(agent.parameters);
+        const sessionTitle = await getTopicTitle(API_TOKEN, agent.parameters.topic);
+
+        if (sessionTitle) {
+            agent.add(`I found the following session based on your input: "${sessionTitle}"`)
+        } else {
+            agent.add(`I found no sessions based on your input.`)
+        }
+    }
+
     // Run the proper function handler based on the matched Dialogflow intent name
     let intentMap = new Map();
-    intentMap.set('show.popular.xkes', showPopularXkes);
     intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('show.popular.xkes', showPopularXkes);
+    intentMap.set('search.topic', searchTopic);
     agent.handleRequest(intentMap);
 };
